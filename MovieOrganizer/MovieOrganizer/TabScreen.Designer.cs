5d11fe2a122fc7aaa96a56312de84c7238407ceb
Malcolm Mergulhao <mmerg_1995@hotmail.com>
2015-11-21 20:19:13 -0600

    updated settings tab

--- MovieOrganizer/MovieOrganizer/TabScreen.Designer.cs
+++ MovieOrganizer/MovieOrganizer/TabScreen.Designer.cs
@@ -38,11 +38,36 @@
             this.collectionTab = new System.Windows.Forms.TabPage();
             this.suggestionsTab = new System.Windows.Forms.TabPage();
             this.settingsTab = new System.Windows.Forms.TabPage();
+            this.applyButton = new System.Windows.Forms.Button();
+            this.pLockPanel = new System.Windows.Forms.Panel();
+            this.pLockBox = new System.Windows.Forms.CheckBox();
+            this.newProfilebutton = new System.Windows.Forms.Button();
+            this.dirPanel = new System.Windows.Forms.Panel();
+            this.addButton = new System.Windows.Forms.Button();
+            this.dirBox = new System.Windows.Forms.TextBox();
+            this.dirLabel = new System.Windows.Forms.Label();
+            this.picturePanel = new System.Windows.Forms.Panel();
+            this.pPictureEditBox = new System.Windows.Forms.PictureBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.picturePathBox = new System.Windows.Forms.TextBox();
+            this.npPanel = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.passwordReLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
             this.uInfoPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.profilePictureBox)).BeginInit();
             this.tabPanel.SuspendLayout();
             this.tabControl.SuspendLayout();
+            this.settingsTab.SuspendLayout();
+            this.pLockPanel.SuspendLayout();
+            this.dirPanel.SuspendLayout();
+            this.picturePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pPictureEditBox)).BeginInit();
+            this.npPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // uInfoPanel
@@ -106,6 +131,7 @@
             // 
             // searchTab
             // 
+            this.searchTab.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.searchTab.Location = new System.Drawing.Point(4, 22);
             this.searchTab.Name = "searchTab";
             this.searchTab.Size = new System.Drawing.Size(889, 515);
@@ -126,6 +152,7 @@
             // 
             // suggestionsTab
             // 
+            this.suggestionsTab.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.suggestionsTab.Location = new System.Drawing.Point(4, 22);
             this.suggestionsTab.Name = "suggestionsTab";
             this.suggestionsTab.Size = new System.Drawing.Size(889, 515);
@@ -135,12 +162,210 @@
             // 
             // settingsTab
             // 
+            this.settingsTab.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.settingsTab.Controls.Add(this.pLockPanel);
+            this.settingsTab.Controls.Add(this.dirPanel);
+            this.settingsTab.Controls.Add(this.picturePanel);
+            this.settingsTab.Controls.Add(this.npPanel);
             this.settingsTab.Location = new System.Drawing.Point(4, 22);
             this.settingsTab.Name = "settingsTab";
             this.settingsTab.Size = new System.Drawing.Size(889, 515);
             this.settingsTab.TabIndex = 3;
             this.settingsTab.Text = "Settings";
             this.settingsTab.UseVisualStyleBackColor = true;
+            this.settingsTab.Enter += new System.EventHandler(this.settingsTab_Enter);
+            // 
+            // applyButton
+            // 
+            this.applyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.applyButton.Location = new System.Drawing.Point(233, 148);
+            this.applyButton.Name = "applyButton";
+            this.applyButton.Size = new System.Drawing.Size(160, 33);
+            this.applyButton.TabIndex = 4;
+            this.applyButton.Text = "Apply";
+            this.applyButton.UseVisualStyleBackColor = true;
+            // 
+            // pLockPanel
+            // 
+            this.pLockPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pLockPanel.Controls.Add(this.pLockBox);
+            this.pLockPanel.Controls.Add(this.newProfilebutton);
+            this.pLockPanel.Location = new System.Drawing.Point(638, 298);
+            this.pLockPanel.Name = "pLockPanel";
+            this.pLockPanel.Size = new System.Drawing.Size(200, 64);
+            this.pLockPanel.TabIndex = 3;
+            // 
+            // pLockBox
+            // 
+            this.pLockBox.AutoSize = true;
+            this.pLockBox.Location = new System.Drawing.Point(2, 42);
+            this.pLockBox.Name = "pLockBox";
+            this.pLockBox.Size = new System.Drawing.Size(193, 17);
+            this.pLockBox.TabIndex = 1;
+            this.pLockBox.Text = "Allow anyone to make a new profile";
+            this.pLockBox.UseVisualStyleBackColor = true;
+            // 
+            // newProfilebutton
+            // 
+            this.newProfilebutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newProfilebutton.Location = new System.Drawing.Point(3, 3);
+            this.newProfilebutton.Name = "newProfilebutton";
+            this.newProfilebutton.Size = new System.Drawing.Size(165, 34);
+            this.newProfilebutton.TabIndex = 0;
+            this.newProfilebutton.Text = "New Profile";
+            this.newProfilebutton.UseVisualStyleBackColor = true;
+            // 
+            // dirPanel
+            // 
+            this.dirPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dirPanel.Controls.Add(this.addButton);
+            this.dirPanel.Controls.Add(this.dirBox);
+            this.dirPanel.Controls.Add(this.dirLabel);
+            this.dirPanel.Location = new System.Drawing.Point(411, 17);
+            this.dirPanel.Name = "dirPanel";
+            this.dirPanel.Size = new System.Drawing.Size(454, 200);
+            this.dirPanel.TabIndex = 2;
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.Location = new System.Drawing.Point(5, 153);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(92, 28);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // dirBox
+            // 
+            this.dirBox.Location = new System.Drawing.Point(5, 30);
+            this.dirBox.Multiline = true;
+            this.dirBox.Name = "dirBox";
+            this.dirBox.Size = new System.Drawing.Size(333, 106);
+            this.dirBox.TabIndex = 1;
+            // 
+            // dirLabel
+            // 
+            this.dirLabel.AutoSize = true;
+            this.dirLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dirLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dirLabel.Location = new System.Drawing.Point(0, 0);
+            this.dirLabel.Name = "dirLabel";
+            this.dirLabel.Size = new System.Drawing.Size(169, 25);
+            this.dirLabel.TabIndex = 0;
+            this.dirLabel.Text = "Movie Locations";
+            this.dirLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // picturePanel
+            // 
+            this.picturePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picturePanel.Controls.Add(this.pPictureEditBox);
+            this.picturePanel.Controls.Add(this.browseButton);
+            this.picturePanel.Controls.Add(this.picturePathBox);
+            this.picturePanel.Location = new System.Drawing.Point(126, 223);
+            this.picturePanel.Name = "picturePanel";
+            this.picturePanel.Size = new System.Drawing.Size(455, 219);
+            this.picturePanel.TabIndex = 1;
+            // 
+            // pPictureEditBox
+            // 
+            this.pPictureEditBox.Location = new System.Drawing.Point(31, 40);
+            this.pPictureEditBox.Name = "pPictureEditBox";
+            this.pPictureEditBox.Size = new System.Drawing.Size(224, 174);
+            this.pPictureEditBox.TabIndex = 2;
+            this.pPictureEditBox.TabStop = false;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browseButton.Location = new System.Drawing.Point(284, -1);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(98, 31);
+            this.browseButton.TabIndex = 1;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // picturePathBox
+            // 
+            this.picturePathBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.picturePathBox.Location = new System.Drawing.Point(3, 3);
+            this.picturePathBox.Name = "picturePathBox";
+            this.picturePathBox.Size = new System.Drawing.Size(271, 31);
+            this.picturePathBox.TabIndex = 0;
+            // 
+            // npPanel
+            // 
+            this.npPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.npPanel.Controls.Add(this.applyButton);
+            this.npPanel.Controls.Add(this.textBox2);
+            this.npPanel.Controls.Add(this.passwordBox);
+            this.npPanel.Controls.Add(this.nameBox);
+            this.npPanel.Controls.Add(this.passwordReLabel);
+            this.npPanel.Controls.Add(this.passwordLabel);
+            this.npPanel.Controls.Add(this.nameLabel);
+            this.npPanel.Location = new System.Drawing.Point(7, 17);
+            this.npPanel.Name = "npPanel";
+            this.npPanel.Size = new System.Drawing.Size(398, 200);
+            this.npPanel.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(233, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 31);
+            this.textBox2.TabIndex = 5;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.passwordBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordBox.Location = new System.Drawing.Point(233, 74);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(160, 31);
+            this.passwordBox.TabIndex = 4;
+            // 
+            // nameBox
+            // 
+            this.nameBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameBox.Location = new System.Drawing.Point(233, 7);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(160, 31);
+            this.nameBox.TabIndex = 3;
+            // 
+            // passwordReLabel
+            // 
+            this.passwordReLabel.AutoSize = true;
+            this.passwordReLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordReLabel.Location = new System.Drawing.Point(3, 111);
+            this.passwordReLabel.Name = "passwordReLabel";
+            this.passwordReLabel.Size = new System.Drawing.Size(207, 25);
+            this.passwordReLabel.TabIndex = 2;
+            this.passwordReLabel.Text = "Password (re-enter):";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(49, 74);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(160, 25);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "New Password:";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(135, 10);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(74, 25);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Name:";
             // 
             // TabScreen
             // 
@@ -151,12 +376,23 @@
             this.Controls.Add(this.uInfoPanel);
             this.Name = "TabScreen";
             this.Text = "TabScreen";
+            this.Resize += new System.EventHandler(this.TabScreen_Resize);
             this.uInfoPanel.ResumeLayout(false);
             this.uInfoPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.profilePictureBox)).EndInit();
             this.tabPanel.ResumeLayout(false);
             this.tabControl.ResumeLayout(false);
+            this.settingsTab.ResumeLayout(false);
+            this.pLockPanel.ResumeLayout(false);
+            this.pLockPanel.PerformLayout();
+            this.dirPanel.ResumeLayout(false);
+            this.dirPanel.PerformLayout();
+            this.picturePanel.ResumeLayout(false);
+            this.picturePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pPictureEditBox)).EndInit();
+            this.npPanel.ResumeLayout(false);
+            this.npPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -173,5 +409,24 @@
         private System.Windows.Forms.TabPage collectionTab;
         private System.Windows.Forms.TabPage suggestionsTab;
         private System.Windows.Forms.TabPage settingsTab;
+        private System.Windows.Forms.Panel npPanel;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Label passwordReLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Panel picturePanel;
+        private System.Windows.Forms.PictureBox pPictureEditBox;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox picturePathBox;
+        private System.Windows.Forms.Panel dirPanel;
+        private System.Windows.Forms.Label dirLabel;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox dirBox;
+        private System.Windows.Forms.Button applyButton;
+        private System.Windows.Forms.Panel pLockPanel;
+        private System.Windows.Forms.CheckBox pLockBox;
+        private System.Windows.Forms.Button newProfilebutton;
     }
 }
--- MovieOrganizer/MovieOrganizer/TabScreen.cs
+++ MovieOrganizer/MovieOrganizer/TabScreen.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Xml;
 namespace MovieOrganizer
 {
     public partial class TabScreen : Form
     {
+        private string username;
+        private string imageLocation;
+
         public TabScreen(string username,string imageLocation)
         {
             InitializeComponent();
+
+            this.username = username;
+            this.imageLocation = imageLocation;
 
             //set up the profile area
             usernameLabel.Text = username;
@@ -24,6 +30,78 @@ namespace MovieOrganizer
             //make the tabe take up the top of the screeen
             tabControl.SizeMode = TabSizeMode.Fixed;
             tabControl.ItemSize = new Size(tabControl.Width / tabControl.TabCount-1, 30); //need to take away 1 so the tabs dont take up too much space.
+        }
+
+        private void TabScreen_Resize(object sender, EventArgs e)
+        {
+            tabControl.ItemSize = new Size(tabControl.Width / tabControl.TabCount - 1, 30);
+        }
+
+        private void settingsTab_Enter(object sender, EventArgs e)
+        {
+            nameBox.Text = username;
+
+            //make the picture corrct for the profile
+            picturePathBox.Text = imageLocation;
+            pPictureEditBox.ImageLocation = imageLocation;
+            pPictureEditBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            loadPaths(dirBox);
+
+        }
+
+        private void loadPaths(TextBox box)
+        {
+            string newText = "";
+            var doc = System.Xml.Linq.XDocument.Load("paths.xml");
+
+            foreach (var element in doc.Element("paths").Elements())
+            {
+                newText += element.Value+Environment.NewLine;
+            }
+
+            box.Text = newText;
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            DialogResult result = fb.ShowDialog();
+
+            if(result == DialogResult.OK)
+            {
+                Console.WriteLine(fb.SelectedPath);
+                addToPath(fb.SelectedPath);
+            }
+
+            loadPaths(dirBox);
+        
+        }
+
+        private void addToPath(string newPath)
+        {
+            var doc = System.Xml.Linq.XDocument.Load("paths.xml");
+            System.Xml.Linq.XElement paths = doc.Element("paths");
+            paths.Add(new System.Xml.Linq.XElement("path", newPath));
+            doc.Save("paths.xml");
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Images (*.jpg)|*.jpg"; //TODO: what about other types of images?
+            DialogResult result = open.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                imageLocation = open.FileName;
+                picturePathBox.Text = imageLocation;
+                profilePictureBox.ImageLocation = imageLocation;
+                pPictureEditBox.ImageLocation = imageLocation;
+                //TODO: update the user's account in xml so the path is current
+            }
+
         }
     }
 }

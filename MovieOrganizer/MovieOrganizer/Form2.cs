dfb45e28f1069200ca77d0e685316a5e542dbbb2
CoxT9 <T.C_1234@Hotmail.com>
2015-11-22 20:17:18 -0600

    New Forms and their implementation.
    Standalone implementation aspects of HomeScreen, HelpModule, MovieDesc
    and NewUser. Data interaction between forms still undefined.

---
+++ MovieOrganizer/MovieOrganizer/Form2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieOrganizer
+{
+    /* TODO: MovieInfo Form
+    - Load dynamic movie data from IMDB -> Queries based on title. Fill labels upon opening
+    - Add tags: (Max Tags?)
+        - Upon Clicking add:
+            - if textbox has text:
+                - tags.add(textbox.text)
+                - textbox.flush
+                */
+
+    // Somehow the name of the film needs to be passed to this form. At that point I can query the IMDB-DB for filling labels.
+    public partial class MovieInfo : Form
+    {
+        public MovieInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // We need to accept XML data from whichever form opened this window. That is where we get the movie name and its info.
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Do we allow multi word tags?
+        {
+            // Add tag to collection
+            if(NewTag.Text.Length > 0) // What if it's just blank spaces?
+            {
+                if(Tags.Text.Equals("Add a tag...") )
+                {
+                    Tags.Text = NewTag.Text;
+                    NewTag.Text = "";
+                }
+                else
+                {
+                    Tags.Text += (", " + NewTag.Text);
+                    NewTag.Text = "";
+                    // Add NewTag.Text to tags of movie
+                }
+            }
+        }
+    }
+}
---
+++ MovieOrganizer/MovieOrganizer/Form3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MovieOrganizer
+{
+    /* TODO: Help Module
+    - Update label text upon selected index changed. Consider a starting label.
+        -Key, Value dictionary of topic -> Description
+    - Manage label dimensions upon filling
+    */
+    public partial class HelpModule : Form
+    {
+        Dictionary<String, String> descriptions;
+        public HelpModule()
+        {
+            InitializeComponent();
+            descriptions = new Dictionary<String, String>();
+
+            // Load Descriptions
+            loadDescriptions(descriptions);
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Change Label Text to be Description of given index changed
+            Description.Text = descriptions[Topics.SelectedItem.ToString()];
+        }
+
+        private void loadDescriptions(Dictionary<String, String> descriptions)
+        {
+            string readText = "";
+            foreach(String name in Topics.Items)
+            {
+                string path = name + ".txt";
+
+                // This text is added only once to the file.
+                if (File.Exists(path))
+                {
+                    // Create a file to write to.
+                   readText = File.ReadAllText(path);
+                }
+                descriptions[name] = readText;
+            }
+        }
+
+        private void HelpModule_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
